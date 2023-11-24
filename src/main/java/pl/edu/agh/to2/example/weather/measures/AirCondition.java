175e381307458b846b6668d7663f931f8e1bd1fb
Grzegorz <grzegorzpiskorski971@gmail.com>
2023-11-24 21:40:27 +0100

    WeatherApiService hopefully finished and a few more things related to measures.

--- src/main/java/pl/edu/agh/to2/example/weather/measures/AirCondition.java
+++ src/main/java/pl/edu/agh/to2/example/weather/measures/AirCondition.java
@@ -1,9 +1,12 @@
 package pl.edu.agh.to2.example.weather.measures;
 import com.fasterxml.jackson.databind.JsonNode;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 public enum AirCondition {
     VERY_GOOD,
     GOOD,
+    OKAY,
     PASSABLE,
     OKAY,
     UNHEALTHY,
