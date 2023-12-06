34e0f8eb909941b8bf21d6985009f8aba322ab91
kkkucia <11.karolinakucia@gmail.com>
2023-12-06 21:29:50 +0100

    Add second location

--- frontend/src/services/formCheck.js
+++ frontend/src/services/formCheck.js
@@ -2,9 +2,9 @@ const isValid = error =>{
     return !(Object.keys(error).length > 0);
 }
 
-const findInputErrors = (errors, label) => {
+const findInputErrors = (errors, id) => {
     return Object.keys(errors)
-    .filter(key => key.includes(label))
+    .filter(key => key.includes(id))
     .reduce((cur, key) => {return Object.assign(cur, {error: errors[key]})}, {});
 }
 
